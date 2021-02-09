78b84dddede15910929243b9609c7f668aae63c9
Kieran <kieran170@hotmail.co.uk>
2021-02-09 21:24:09 +0000

    added comments to articles

--- nc-news-app/src/api.js
+++ nc-news-app/src/api.js
@@ -60,31 +60,44 @@ export const postVote = (id, article_id) => {
 };
 
 export const getArticlesByTopic = (topic) => {
-  if (topic === 'homepage') {
-    return axios.get('https://kieran-nc-news-app.herokuapp.com/api/articles').then(({data}) => {
-      return data.articles;
-    })
+  if (topic === "homepage") {
+    return axios
+      .get("https://kieran-nc-news-app.herokuapp.com/api/articles")
+      .then(({ data }) => {
+        return data.articles;
+      });
   }
-  return axios.get(`https://kieran-nc-news-app.herokuapp.com/api/articles?topic=${topic}`).then(({data}) => {
-    return data.articles;
-  })
-}
+  return axios
+    .get(`https://kieran-nc-news-app.herokuapp.com/api/articles?topic=${topic}`)
+    .then(({ data }) => {
+      return data.articles;
+    });
+};
 
 export const getArticleById = (article_id) => {
-  return axios.get(`https://kieran-nc-news-app.herokuapp.com/api/articles/${article_id}`).then(({data})=> {
-    return data.article;
-  })
-}
+  return axios
+    .get(`https://kieran-nc-news-app.herokuapp.com/api/articles/${article_id}`)
+    .then(({ data }) => {
+      return data.article;
+    });
+};
 
 export const getArticleComments = (article_id) => {
-  return axios.get(`https://kieran-nc-news-app.herokuapp.com/api/articles/${article_id}/comments`).then(({data}) => {
-    
-  })
-}
+  return axios
+    .get(
+      `https://kieran-nc-news-app.herokuapp.com/api/articles/${article_id}/comments`
+    )
+    .then(({ data }) => {
+      return data.comments;
+    });
+};
 
 export const getArticlesByAuthor = (author) => {
-  return axios.get(`https://kieran-nc-news-app.herokuapp.com/api/articles?author=${author}`).then(({data})=> {
-    return data.articles;
-  })
-}
-
+  return axios
+    .get(
+      `https://kieran-nc-news-app.herokuapp.com/api/articles?author=${author}`
+    )
+    .then(({ data }) => {
+      return data.articles;
+    });
+};
--- nc-news-app/src/components/ArticleById.jsx
+++ nc-news-app/src/components/ArticleById.jsx
@@ -17,21 +17,37 @@ class ArticleById extends Component {
         this.fetchArticleComments(this.props.article_id)
     }
     render() {
-        
         const {body, title, author, votes, comment_count} = this.state.article
-        const {errMessage} = this.state
+        const {errMessage, comments} = this.state
         if(this.state.isLoading === true) {
             return <img className='loading' src='https://media4.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif?cid=ecf05e47jsvriuti9367kth1dv181fu679bvvaj9ock6ptyl&rid=giphy.gif' alt='loading' />
         }
         if(errMessage) {return <ErrorDisplayer msg={errMessage} />}
         return (
-            <div className='single-article-container'>
-                <h2 className='single-article-title'>{title}</h2>
-                <p className='single-article-body' >{body}</p>
-                <p className='single-author'>Author: {author}</p>
-                <p className='single-comments'>Votes: {votes}</p>
-                <p className='single-comments'>Comments: {comment_count}</p>
-            </div>
+            <>
+                <div className='single-article-container'>
+                    <h2 className='single-article-title'>{title}</h2>
+                    <p className='single-article-body' >{body}</p>
+                    <p className='single-author'>Author: {author}</p>
+                    <p className='single-comments'>Votes: {votes}</p>
+                    <p className='single-comments'>Comments: {comment_count}</p>
+              </div>
+              <ul className='all-comments-container'>
+                  <h2 className='comment-title'>Comments</h2>
+                  {comments.map((comment) => {
+                      return(
+                          <>
+                            <li className='single-comment-container' key={comment.comment_id}>
+                                <p className='single-comment' >{comment.body}</p>
+                                <p className='comment-vote'>Votes: {comment.votes}</p>
+                                <button>👍</button>
+                                <button>👎</button>
+                            </li>
+                          </>
+                      )
+                  })}
+              </ul>
+            </>
         );
     }
     fetchArticleById = (article_id) => {
@@ -42,7 +58,9 @@ class ArticleById extends Component {
         })
     }
     fetchArticleComments = (article_id) => {
-        api.getArticleComments(article_id)
+        api.getArticleComments(article_id).then((comments) => {
+            this.setState({comments, isLoading: false})
+        })
     }
 }
 
