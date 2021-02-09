c5ff5a218f0e21f4b833667994f4f37fbce38470
kieran cookson <kieran170@hotmail.co.uk>
2021-02-09 15:20:53 +0000

    starting article singular

--- nc-news-app/src/components/ArticleById.jsx
+++ nc-news-app/src/components/ArticleById.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const ArticleById = (props) => {
+    console.log(props)
     return (
         <div>
             <p>Hello By id</p>
--- nc-news-app/src/components/ArticlesCard.jsx
+++ nc-news-app/src/components/ArticlesCard.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import LikeCount from './LikeCount'
-import {Link} from '@reach/router'
+import {Link, Router} from '@reach/router'
 
 const ArticlesCard = (props) => {
     return (
         <section className='article-card'>
-            <Link to={`/article/${props.article_id}`} className='article-title'>{props.title}</Link>
+            <Link to={`/article/${props.article_id}`} article={props} className='article-title'>{props.title}</Link>    
             <p>Topic: {props.topic}</p>
             <Link to={`/articles-by-author/${props.author}`}>Author: {props.author}</Link>
             <p>{props.created_at}</p>
