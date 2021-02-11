a58513159a253ca30bc420881446bf38eb295a73
kieran cookson <kieran170@hotmail.co.uk>
2021-02-11 10:26:42 +0000

    added CSS

--- nc-news-app/src/components/AddComment.jsx
+++ nc-news-app/src/components/AddComment.jsx
@@ -17,9 +17,9 @@ class AddComment extends Component {
             this.setState({newComment: ''})
             }} id='commentForm'>
             <h2 className='post-comment-title' >Post A Comment</h2>
+            {this.state.failedRegex === true && newComment === '' ? <p className='failed-regex-text'>Please add text</p> : null}
             <textarea className='comment-box' rows='4' cols='43' name='comment' form='commentForm' placeholder='Add comment here' value={newComment} onBlur={this.handleBlur} onChange={this.handleChange}></textarea>
             <button className='post-comment-button'>Submit Comment</button>
-            {this.state.failedRegex === true && newComment === '' ? <p>Please add text</p> : null}
         </form>
         );
     }
